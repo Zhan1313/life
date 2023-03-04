805252f40c279238c1f2e534eb79f1408125f0ad
Zhan1313 <zhan.k1313@gmail.com>
2023-03-04 20:37:09 +0600

    commit

--- function.js
+++ function.js
@@ -7,15 +7,17 @@ const numberOfNeighbors = (rowNumber, columnNumber) => {
             [0, 1, 1, 0, 0]];
     let neighbors = 0;
     let height = field.length;
+    let rowIndex = rowNumber - 1;
+    let colIndex = columnNumber - 1;
 
-    if (field[rowNumber - 1][(columnNumber - 1) - 1] === 1) {
+    if (field[rowIndex][colIndex - 1] === 1) {
         neighbors += 1;
-    } else if (field[rowNumber - 1][(columnNumber - 1) - 1] === 0
-        || field[rowNumber - 1][(columnNumber - 1) - 1] === undefined) {
+    } else if (field[rowIndex][colIndex - 1] === 0
+        || colIndex - 1 < 0) {
         neighbors += 0;
     }
     return neighbors;
 }
-
 numberOfNeighbors(1, 1) // 0
 numberOfNeighbors(2, 4) // 1
+numberOfNeighbors(1,4) // 0
