7e99f7cc0e86a24512e58e5fe0652a42a1abdf54
Zhan1313 <zhan.k1313@gmail.com>
2023-03-07 17:50:28 +0600

    update

--- function.js
+++ function.js
@@ -5,10 +5,18 @@ const getNextGeneration = (array) => {
             let arrayRow = i + 1;
             let arrayCol = j + 1;
             let neighbors = getNumberOfNeighbors(array, arrayRow, arrayCol);
-            if (array[i][j] === 0 && neighbors === 3) {
-                newArray[i][j] = 1;
-            } else if (array[i][j] === 1 && (neighbors < 2 || neighbors > 3)) {
-                newArray[i][j] = 0;
+            if (array[i][j] === 1) {
+                if (neighbors === 2 || neighbors === 3) {
+                    newArray[i][j] = 1;
+                } else if (neighbors < 2 || neighbors > 3) {
+                    newArray[i][j] = 0;
+                }
+            } else if (array[i][j] === 0) {
+                if (neighbors === 3) {
+                    newArray[i][j] = 1;
+                } else {
+                    newArray[i][j] = 0;
+                }
             }
         }
     }
